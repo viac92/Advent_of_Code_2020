e3b07c0ca8b58f7cc91766370e20fa19f4671b4e
viac92 <64146594+viac92@users.noreply.github.com>
2020-12-08 21:39:34 +0100

    begining of vs 2

--- 07/firstHalf_day7/main.go
+++ 07/firstHalf_day7/main.go
@@ -14,8 +14,6 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strconv"
-	"strings"
 )
 
 type bag struct {
@@ -23,93 +21,14 @@ type bag struct {
 	name string
 }
 
-func createBag(str []string) []bag {
-	var bagOut []bag
-	var num int
-	if len(str) == 7 {
-		bagOut = append(bagOut, bag{0, str[1] + " " + str[0]})
-		return bagOut
-	}
-	for i := len(str) - 2; i > 0; i -= 4 {
-		if i == 1 {
-			num = 0
-		} else {
-			num, _ = strconv.Atoi(str[i-2])
-		}
-		bagOut = append(bagOut, bag{num, str[i-1] + " " + str[i]})
-		if i == 6 {
-			i--
-		}
-	}
-	return bagOut
-}
-
 func main() {
 	b := bufio.NewScanner(os.Stdin)
-	var bagSlice []bag
-	var canGold []bag
-	var sliceT []string
 
-	counter := 0
 	for b.Scan() {
-		counter++
 		t := b.Text()
-		rowSlice := strings.Split(t, " ")
-		bagSlice = createBag(rowSlice)
-		sliceT = append(sliceT, t)
-		fmt.Println(counter)
-		fmt.Println(bagSlice)
-
-		flag := false
-		for i := 0; i < len(bagSlice); i++ {
-			if flag {
-				canGold = append(canGold, bagSlice[i])
-			}
-			if  bagSlice[i].name == "shiny gold" {
-				flag = true
-			}
-		}
-		if flag {
-			fmt.Println(canGold)
-			fmt.Println()
-		}
+		fmt.Println(t)
+		
 	}
-
-	var bagContainGold []string
-	goldBag := 0
-	for _,r := range sliceT {
-		for _,k := range canGold {
-			if strings.Contains(r, k.name) {
-				rowSlice := strings.Split(r, " ")
-				bagContainGold = append(bagContainGold, rowSlice[0] + " " + rowSlice[1])
-				goldBag++
-				break
-			}
-		}
-	}
-
-	var bagContainGold2 []string
-	for _,r := range sliceT {
-		for _,k := range bagContainGold {
-			if strings.Contains(r, k) {
-				rowSlice := strings.Split(r, " ")
-				bagContainGold = append(bagContainGold2, rowSlice[0] + " " + rowSlice[1])
-				goldBag++
-				break
-			}
-		}
-	}
-
-
-
-	fmt.Println(canGold)
-	fmt.Println(len(canGold))
-	fmt.Println(goldBag)
-	fmt.Println(bagContainGold)
-	fmt.Println(len(bagContainGold))
-	fmt.Println(bagContainGold2)
-	fmt.Println(len(bagContainGold2))
 }
 //[{3 clear tan} {0 dull white} {4 drab tomato} {0 clear turquoise} {2 dark coral} {0 wavy lime} {4 wavy coral} {0 striped green} {0 shiny salmon} {0 plaid gray}]
 
-// ciao ciao 
