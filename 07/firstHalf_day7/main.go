a1d9750d24fa41cfde52390c8fb4ffdff5966b88
viac92 <64146594+viac92@users.noreply.github.com>
2020-12-13 13:56:05 +0100

    solve first half day 7

--- 07/firstHalf_day7/main.go
+++ 07/firstHalf_day7/main.go
@@ -14,21 +14,99 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
+	"strconv"
+	"regexp"
 )
 
 type bag struct {
-	num  int
-	name string
+	numberOfBag int
+	bagName string
+}
+
+type bagRule struct {
+	ruleName string
+	bagConteined []bag
+}
+
+func parseLine(line string) (bRule bagRule, err error) {
+	var b bag
+	var nameSlice []string
+	nameBagRule := regexp.MustCompile("([a-z])+")
+	nameSlice = nameBagRule.FindAllString(line, 2)
+	bRule.ruleName = nameSlice[0] + " " + nameSlice[1]
+
+
+	if strings.ContainsAny(line, "0123456789") {
+		
+		numberBagConteinde := regexp.MustCompile("[0-9]")	
+		nameBagContained := regexp.MustCompile("([a-z])+")
+		for strings.ContainsAny(line, "0123456789") {
+			
+			b.numberOfBag,_ = strconv.Atoi(numberBagConteinde.FindString(line))			
+			line = line[numberBagConteinde.FindStringIndex(line)[0] + 1:]
+			s := nameBagContained.FindAllString(line, 2) 
+			b.bagName = s[0] + " " + s[1] 
+					
+			bRule.bagConteined = append(bRule.bagConteined, b)
+		}
+	}
+	return
 }
 
 func main() {
 	b := bufio.NewScanner(os.Stdin)
+	var bRule []bagRule
+	var okBag []string
+	var okBagCounter int
 
 	for b.Scan() {
 		t := b.Text()
-		fmt.Println(t)
-			
+		bR,_ := parseLine(t) 
+		bRule = append(bRule, bR)
 	}
-}
-//[{3 clear tan} {0 dull white} {4 drab tomato} {0 clear turquoise} {2 dark coral} {0 wavy lime} {4 wavy coral} {0 striped green} {0 shiny salmon} {0 plaid gray}]
 
+	for _,bR := range bRule {
+		for _,bg := range bR.bagConteined {
+			if bg.bagName == "shiny gold" {
+				okBag = append(okBag, bR.ruleName)
+				okBagCounter++
+				break
+			}
+		}
+	}
+
+	var count int
+	for {
+		fmt.Println(okBag[count:])
+		for _,ok := range okBag[count:] {
+			count++
+			for _,bR := range bRule {
+				loop: for _,bg := range bR.bagConteined {
+					if ok == bg.bagName {
+						for _,allOk := range okBag {
+							if allOk == bR.ruleName {
+								break loop
+							}
+						}
+						okBag = append(okBag, bR.ruleName)
+						okBagCounter++
+						break
+					}
+				}
+			}
+		}
+		if count == len(okBag) {
+			break
+		}
+		fmt.Println(count)
+	}
+
+	// fmt.Println(bRule)
+	// fmt.Println()
+	for _,r := range okBag {
+	 fmt.Println(r)
+	}
+	fmt.Println(len(okBag))
+	// fmt.Println(okBagCounter)
+}
